9173197021692ed820573a6a5a261038e9b0fa44
Faisal <tuneinfaisal@gmail.com>
2021-11-23 11:10:52 +0530

    Version - 1.2 (adding date feature)

--- todo.js
+++ todo.js
@@ -26,7 +26,11 @@ addBtn.onclick = () => {
     else {
         listArr = JSON.parse(getLocalStorage);//converting json string into a js object
     }
-    listArr.push(userData); //adding user data
+    // listArr.push(userData); //adding user data
+    // const time = new moment();
+    // listArr.push(userData + " " + time.getHours() + ":" + time.getMinutes());
+    listArr.push(userData + " (" + moment().format("MMM Do YY") + " - " + moment().calendar() + ")");//adding date and relative time with moment.js
+    // listArr.push(userData + " (" + moment().format("MMM Do YY") + " - " + moment().startOf('hour').fromNow() + ")");//adding moments.js
     localStorage.setItem("New Todo", JSON.stringify(listArr)); //converting js object into json string
     addBtn.classList.remove("active");
     showTasks();
